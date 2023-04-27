03d225ba9b05ca0191e13d45a58512df7b476320
Constantin Cuciurcă <constantin@codegod.dev>
2023-04-27 23:15:04 +0300

    fix: test liveness probe

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const { createClient } = require("redis");
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
